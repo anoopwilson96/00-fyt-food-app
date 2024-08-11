e991e6786c79b5dbf94c0c23f2c739a3442c7ffa
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-11 22:28:55 +0530

    Create index.js

---
+++ Server/routes/v1/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const v1Router = express.Router()
+
+
+
+
+
+
+
+
+// v1Router.use('/trail',(req,res)=>{
+//   try {
+//     res.send("Its working" )
+//   } catch (error) {
+//     res.send('Error')
+//   }})
+
+
+export default v1Router
