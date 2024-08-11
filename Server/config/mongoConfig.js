b3c0496fd481fd7284201b2b13bbda01b0d56df8
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-11 22:28:37 +0530

    Create mongoConfig.js

---
+++ Server/config/mongoConfig.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+
+export const  databaseConfig = ()=>{
+
+    main().catch(err => console.log(err));
+
+    async function main() {
+         await mongoose.connect(process.env.DATABASE_URL)
+         console.log("connected");
+         } 
+  }
+  
