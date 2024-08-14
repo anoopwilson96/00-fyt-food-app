348125f2589b57889f11a64db3122b18ad0cf806
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-14 19:06:15 +0530

    Update index.js

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -6,8 +6,11 @@ const apiRouter = express.Router();
 
 
 //Router redirected to index.js(v1)
-// localhost3000:api/v1/(needed routes) this is how url will be.
+
 apiRouter.use('/v1',v1Router)
+// apiRouter.use('/v2', v1Router )
+// apiRouter.use('/v3', v1Router )
+// localhost3000:api/v1/(needed routes) this is how url will be.
 
 
 
