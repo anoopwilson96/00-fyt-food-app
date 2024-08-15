699368ef743104976ac0e27973fb8f82c8489b70
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-15 23:23:38 +0530

    Update index.js

--- Server/routes/v1/index.js
+++ Server/routes/v1/index.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import userRoute from './userRoute.js'
+import dishRoute from './dishRoute.js'
+import menuItemRoute from './menuItemRoute.js'
+import restaurantRoute from './restaurantRoute.js'
 
 const v1Router = express.Router()
 
-v1Router.use('/add',userRoute)
+v1Router.use('/user',userRoute)
+v1Router.use('/dish',dishRoute)
+v1Router.use('/menu-item',menuItemRoute)
+v1Router.use('/restaurant',restaurantRoute)
 
-export default v1Router
+
+export default v1Router 
