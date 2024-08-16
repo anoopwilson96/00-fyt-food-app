2ce2f5587f3f14fa60376d2f1aa26c14a9507722
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-16 17:40:35 +0530

    Update userRoute.js

--- Server/routes/v1/userRoute.js
+++ Server/routes/v1/userRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import {addUser,userLogin, userLogout} from '../../controllers/userController.js'
+import {addUser,userLogin, userLogout, userProfile} from '../../controllers/userController.js'
+import { authUser } from '../../middlewares/authUser.js'
 const router = express.Router()
 
 router.get('/test',(req,res)=>{ res.send('This is a test request')})
@@ -7,6 +8,7 @@ router.get('/test',(req,res)=>{ res.send('This is a test request')})
 router.post('/add',addUser)
 router.post('/login',userLogin)
 router.post('/logout',userLogout)
+router.get('/profile/:id',authUser,userProfile)
 
 
 
