63f5fc65030b17ab1731c1a6304e0c26b71a1c58
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-16 23:43:04 +0530

    Update restaurantRoute.js

--- Server/routes/v1/restaurantRoute.js
+++ Server/routes/v1/restaurantRoute.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import addRestaurant from '../../controllers/restaurantController.js'
+import { addRestaurant,getRestaurant,getAllRestaurants,deleteRestaurant,updateRestaurant } from '../../controllers/restaurantController.js';
+
 const router = express.Router()
 
 router.get('/test',(req,res)=>{ res.send('This is a test request')});
 
 router.post('/add',addRestaurant);
+router.get('/restaurant/:id',getRestaurant)
+router.get('/all-restaurants',getAllRestaurants)
+router.post('/delete',deleteRestaurant)
+router.patch('/update',updateRestaurant)
+
 
 
 export default router
