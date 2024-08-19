5eb00eeefccaa7608f124d1b1ac30d50a5283f1c
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-20 01:42:24 +0530

    Create adminRoute.js

---
+++ Server/routes/v1/adminRoute.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import {addUser,checkUser,userLogin, userLogout, userProfile} from '../../controllers/userController.js'
+import { authUser } from '../../middlewares/authUser.js'
+import { upload } from '../../middlewares/uploadFile.js';
+import { addAdmin, adminLogin, adminLogout,adminUpdate,checkAdmin } from '../../controllers/adminController.js';
+import { authAdmin } from '../../middlewares/authAdmin.js';
+const router = express.Router()
+
+router.get('/test',authAdmin,(req,res)=>{ res.send('This is a test request')})
+
+router.post('/add',addAdmin)
+router.post('/login',adminLogin)
+router.post('/logout',adminLogout)
+router.patch('update/:id',authAdmin,adminUpdate)
+
+
+// For front end
+router.get("/check-admin", authAdmin,checkAdmin);
+
+
+export default router
