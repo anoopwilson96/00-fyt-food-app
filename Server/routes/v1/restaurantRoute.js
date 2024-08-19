4515cda33897315c779a92a470b00a8cae409a1a
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-20 01:42:37 +0530

    Update restaurantRoute.js

--- Server/routes/v1/restaurantRoute.js
+++ Server/routes/v1/restaurantRoute.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import { addRestaurant,getRestaurant,getAllRestaurants,deleteRestaurant,updateRestaurant } from '../../controllers/restaurantController.js';
 import { upload } from '../../middlewares/uploadFile.js';
+import {authAdmin} from '../../middlewares/authAdmin.js'
 import multer from 'multer';
 
 const router = express.Router()
 
 router.get('/test',(req,res)=>{ res.send('This is a test request')});
 
-router.post('/add',upload.single('image'),addRestaurant);
+router.post('/add',authAdmin,upload.single('image'),addRestaurant);
 router.get('/get/:id',getRestaurant)
 router.get('/all',getAllRestaurants)
-router.post('/delete',deleteRestaurant)
-router.patch('/update/:id',upload.single('image'),updateRestaurant)
+router.post('/delete',authAdmin,deleteRestaurant)
+router.patch('/update/:id',upload.single('image'),authAdmin,updateRestaurant)
 
 
 
