1fde9c19f96204b4b6d2f77a4f585c7ed0e613d2
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-19 19:29:04 +0530

    Update cloudinaryUpload.js

--- Server/utils/cloudinaryUpload.js
+++ Server/utils/cloudinaryUpload.js
@@ -4,11 +4,17 @@ import { cloudinaryInstance } from "../config/cloudinaryConfig.js";
 export const imageUploadCloudinary = async (path) => {
   
     try {
-        const uploadResult = await cloudinaryInstance.uploader.upload(path).catch((error) => {
+        const uploadResult = await cloudinaryInstance.uploader.upload(path,{ folder: '00-fyt-food-website' }).catch((error) => {
             console.error(error);
         });
-        return uploadResult.url;
+        
+        return  uploadResult.url;
+
     } catch (error) {
-        res.status(error.status || 500).json({ message: error.message || "Internal server error" });
-    }
+        console.log(error)
+
+     }
 };
+
+
+
