6893c910f06f660873ff9ae8230a81a1099e2401
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-19 19:30:45 +0530

    Update menuItemModel.js

--- Server/models/menuItemModel.js
+++ Server/models/menuItemModel.js
@@ -4,21 +4,25 @@ const { Schema } = mongoose;
 
 
 const menuItemSchema = new Schema({
-  name: {
+  name:{
     type: String,
-    required: true
+    
   },
   description: {
     type: String
   },
-  price: {
-    type: Number,
-    required: true
-  },
-  restaurant: {
+  restaurant: [{
     type: Schema.Types.ObjectId,
     ref: 'Restaurant'
-  }
+  }],
+dish: [{
+  type: Schema.Types.ObjectId,
+  ref: 'Dish'
+}],
+image: {
+  type: String,
+  default: "https://res.cloudinary.com/aw96/image/upload/v1723921863/00-fyt-food-website/plceholder%20dish%20icon.jpg"
+}
 });
 
 export const MenuItem = mongoose.model('MenuItem', menuItemSchema);
