7533d1f346a94f2e636d4057b3b29491d68c5f35
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-20 01:42:42 +0530

    Update generateToken.js

--- Server/utils/generateToken.js
+++ Server/utils/generateToken.js
@@ -5,3 +5,9 @@ export const generateUserToken = (email)=>{
   const token = jwt.sign({email:email,role:'user'},'process.env.JWT_SK')
   return token
 }
+
+
+export const generateToken = (email,role)=>{
+  const token = jwt.sign({email: email,role: role || 'none' },'process.env.JWT_SK')
+  return token
+}
