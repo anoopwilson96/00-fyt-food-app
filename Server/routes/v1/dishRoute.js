a4ff333ef2c66f45253d1bcb540e2c93bcf54134
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-19 19:31:30 +0530

    Update dishRoute.js

--- Server/routes/v1/dishRoute.js
+++ Server/routes/v1/dishRoute.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import addDish from '../../controllers/dishController.js'
+import {addDish,getDish,getAllDishes} from '../../controllers/dishController.js'
+import { upload } from '../../middlewares/uploadFile.js';
 const router = express.Router()
 
 router.get('/test',(req,res)=>{ res.send('This is a test request')});
 
-router.post('/add',addDish);
+router.post('/add',upload.single('image'),addDish);
+router.get('/get/:id',getDish)
+router.get('/all',getAllDishes)
+router.patch('/update/:id',upload.single('image'),getAllDishes)
 
 
 export default router 
