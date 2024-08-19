859ed169a83a4d181413eda205345923c1ebfe40
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-19 19:31:33 +0530

    Update menuItemRoute.js

--- Server/routes/v1/menuItemRoute.js
+++ Server/routes/v1/menuItemRoute.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import addMenuItem from '../../controllers/menuItemController.js'
+import { addMenuItem, getAllMenuItems, getMenuItem,updateMenuItem } from '../../controllers/menuItemController.js';
+import { upload } from '../../middlewares/uploadFile.js';
 const router = express.Router()
+
 
 router.get('/test',(req,res)=>{ res.send('This is a test request')});
 
-router.post('/add',addMenuItem);
+router.post('/add',upload.single('image'),addMenuItem);
+router.get('/get/:id',getMenuItem);
+router.patch('/update',updateMenuItem);
+router.get('/all',getAllMenuItems);
+
 
 
 export default router
