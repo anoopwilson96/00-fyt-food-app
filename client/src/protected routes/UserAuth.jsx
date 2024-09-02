7f8e0ce545771d6533f2995369495fd986d1a4b3
Anoop Wilson <anoopwilson33@yahoo.com>
2024-09-02 22:51:14 +0530

    Update UserAuth.jsx

--- client/src/protected routes/UserAuth.jsx
+++ client/src/protected routes/UserAuth.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { axiosInstance } from '../config/axiosInstance';
+import { useNavigate, useLoaderData } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { useState } from 'react';
+
 
 export const UserAuth = ({children}) => {
- 
-  const checkUser = async()=>{
+const navigate = useNavigate()
+const [user, setUser] = useState();
+
+
+ const checkUser = async()=>{
+  
+    try {
+      const response = await axiosInstance({
+        url: "/user/check-user",
+        method: "GET",
+        withCredentials:true 
+      })
+    setUser(true)
+    } catch (error) {
+      toast.error('Login To Continue')
+      navigate('/login')
+      console.log(error)
+      
+    }
 
   }
 
-  return ;
-  
+  useEffect(()=>{
+    checkUser()
+  },[location.pathname])
+
+
+  return user ? children : null;
+
 }
+
+
