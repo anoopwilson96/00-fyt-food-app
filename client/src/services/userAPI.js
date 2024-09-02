de3ca28628462b8e4b2675155039f9eb327bf855
Anoop Wilson <anoopwilson33@yahoo.com>
2024-09-02 22:51:10 +0530

    Update userAPI.js

--- client/src/services/userAPI.js
+++ client/src/services/userAPI.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { axiosInstance } from "../config/axiosInstance";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -20,14 +22,63 @@ export const userLogin = async (data)=>{
 }
 
 
-export const checkUser = async ()=>{
+
+export const UserLogout = async ()=>{
+
   try {
     const response = await axiosInstance({
-      url: "/user/user-check",
-      method: "GET",
+      url: "/user/logout",
+      method: "POSt",
+      withCredentials: true
     })
-
+    if(response){
+      console.log(response)
+      toast.success('Logged out successfully')
+      
+    }
+    return 
   } catch (error) {
-    console.log(error)
+    toast.error('Error: Try again')
+    
   }
 }
+
+//fetch user data 
+
+export const fetchUser = async ()=>{
+  try {
+    const  response = await axiosInstance({
+      url: "/user/profile" ,
+      method: "GET",
+      withCredentials: true 
+    })
+  console.log(response,"====response")
+  return response?.data?.data;
+  } catch (error) {
+    console.log("Error fetching user data")
+  }
+}
+
+
+
+
+
+
+
+
+
+// check user login at "/"
+
+  export const IsLoggedIn = async () => {
+    try {
+      const response = await axiosInstance({
+        url: "/user/check-user",
+        method: "GET",
+        withCredentials: true
+      });
+      return response?.data; // Return the response data
+    } catch (error) {
+      console.log("Please Login");
+      return null; // Return null in case of error
+    }
+  };
