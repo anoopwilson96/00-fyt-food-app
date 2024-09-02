179b48bb3e85377a5410d581e558d4d63fb1d492
Anoop Wilson <anoopwilson33@yahoo.com>
2024-09-03 00:22:47 +0530

    Update profile.jsx

--- client/src/pages/user/profile.jsx
+++ client/src/pages/user/profile.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchUser } from '../../services/userAPI'
+import toast from 'react-hot-toast';
+import { useState} from 'react';
 
-export const Profile = () => {
+export const Profile =  () => {
+  const [user, setUser] = useState({});
 
 
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const userData = await fetchUser();
+        setUser(userData);
+      } catch (error) {
+        console.error(error);
+        toast.error('Error in fetching data');
+      }
+    };
 
-
-
+    getUserData();
+  }, []);
 
 
   return (
 <div className='bg-base-200 p-10 '>
 <div className="max-w-md mb-10 mt-10 mx-auto bg-base-100 p-6 rounded-lg shadow-lg">
-      <h2 className="text-2xl font-semibold text-center mb-4">User's Profile</h2>
+      <h2 className="text-2xl font-semibold text-center mb-4">{user?.name}'s Profile</h2>
       
       <div className="flex flex-col items-center">
         <div className="avatar">
-          <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" alt="Profile" />
+          <div className="w-36 rounded">
+            <img src= {user?.image ||"https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"} alt="Profile" />
           </div>
         </div>
         
         <div className="mt-10 mb-10 text-left">
-          <p className="text-gray-600 text-lg font-medium">User's Name</p>
-          <p className="text-gray-600 text-lg font-medium">User's Phone</p>
-          <p className="text-gray-600 text-lg font-medium">User's Email</p>
+          <p className="text-gray-600 text-lg font-medium">{user?.name}</p>
+          <p className="text-gray-600 text-lg font-medium">{user?.email}</p>
+          <p className="text-gray-600 text-lg font-medium">{user?.mobile}</p>
         </div>
         
         <button className="btn btn-primary mb-5">Edit Details</button>
