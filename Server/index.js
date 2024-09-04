17f0e28338394ab7611884f6c3f7ca120ff476b9
Anoop Wilson <anoopwilson33@yahoo.com>
2024-09-04 15:34:31 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ import cookieParser from 'cookie-parser';
 const app = express()
 
 app.use(cors({
- origin: "http://localhost:5173",  
+ origin: "http://localhost:5174",  
  credentials: true
 }))
 app.use(express.json())
