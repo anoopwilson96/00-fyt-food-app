916e0f88defe03f44fa7a0dea1d3f6dc89613646
Anoop Wilson <anoopwilson33@yahoo.com>
2024-09-10 08:15:11 +0530

    Update header.jsx

--- client/src/pages/user/header.jsx
+++ client/src/pages/user/header.jsx
@@ -89,7 +89,7 @@ export const Header = () => {
           <div tabIndex={0} className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
             <div className="card-body">
               <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
+              <span className="text-slate-800 font-medium">Subtotal: $999</span>
               <div className="card-actions">
                 <Link to={'/user/cart'}>
                   <button className="btn btn-primary btn-block">View cart</button>
