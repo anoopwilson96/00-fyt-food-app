c0f887d5fde17ff66d204b6a8a498586160a0fd6
Anoop Wilson <anoopwilson33@yahoo.com>
2024-09-17 21:49:00 +0530

    updated Link in menu item slider

--- client/src/components/menuItemsCard.jsx
+++ client/src/components/menuItemsCard.jsx
@@ -9,11 +9,13 @@ export const MenuItemsCard = ({items}) => {
     <>
       <div className='MenuItems mb-10 avatar flex flex-col items-center align-middle justify-center'>
             <div className=" w-24 rounded-full object-cover mb-3 ">
-              <Link to={`menu-item/${items?._id}`}>
+              
                 <img src= {items?.image} /> 
-              </Link> 
+          
             </div>
+            <Link to={`menu-item/${items?._id}`}>
             <p className='text-lg text-nowrap font-semibold flex items-center '>{items?.name}</p>
+            </Link> 
         </div>
         
     </>
